1455f002cfa337dc7b1492fb0f75a4a51ab94811
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-05-31 14:17:11 +0300

    Do some functions in createGameboard

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const createShip = (length) => {
   return {
     length,
-    timesHit,
-    sunk,
-    hit: () => timesHit++,
-    isSunk: () => length == timesHit,
+    timesHit: 0,
+    sunk: false,
+    hit() {
+      this.timesHit++;
+      if (this.length === this.timesHit) {
+        this.sunk = true;
+      }
+    },
+    isSunk() {
+      return this.sunk;
+    },
+  };
+};
+
+const createGameboard = (nrRows, nrCols) => {
+  return {
+    board: Array.from({ length: nrRows }, () => new Array(nrCols).fill(null)),
+    ships: [],
+    placeShip(ship, topRow, topCol, horz) {
+      if (
+        (horz && (topRow > nrRows || topCol + ship.length > nrCols)) ||
+        (!horz && (topRow + ship.length > nrRows || topCol > nrCols))
+      ) {
+        throw new Error("Illegal ship placement");
+      }
+      if (horz) {
+        for (let i = 0; i < ship.length; i++) {
+          if (this.board[topRow][topCol + i] !== null) {
+            throw new Error("Illegal ship placement");
+          }
+          this.board[topRow][topCol + i] = "shipUnsunk";
+        }
+      } else {
+        for (let i = 0; i < ship.length; i++) {
+          if (this.board[topRow + i][topCol] !== null) {
+            throw new Error("Illegal ship placement");
+          }
+          this.board[topRow + i][topCol] = "shipUnsunk";
+        }
+      }
+      this.ships.push(ship);
+    },
   };
 };
 
 module.exports = {
   createShip,
+  createGameboard,
 };
--- index.test.js
+++ index.test.js
@@ -1,1 +1,21 @@
 const index = require("./index.js");
+
+test("test createShip", () => {
+  const ship = index.createShip(2);
+  expect(ship.length).toBe(2);
+  expect(ship.timesHit).toBe(0);
+  ship.hit();
+  expect(ship.timesHit).toBe(1);
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("test createGameboard", () => {
+  const gameboard = index.createGameboard(2, 2);
+  expect(gameboard.board.length).toBe(2);
+  expect(gameboard.board[0].length).toBe(2);
+  const ship = index.createShip(1);
+  gameboard.placeShip(ship, 0, 0, true);
+  expect(gameboard.board[0][0]).toBe("shipUnsunk");
+  expect(gameboard.board[1][0]).toBe(null);
+});
