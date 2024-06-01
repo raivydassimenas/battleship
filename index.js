f3531221ddd07207ef7c23de2731f764df92ecc1
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-06-01 17:49:12 +0300

    Implement receiveAttack

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const createShip = (length) => {
     length,
     timesHit: 0,
     sunk: false,
+    coords: [],
     hit() {
       this.timesHit++;
       if (this.length === this.timesHit) {
@@ -31,18 +32,38 @@ const createGameboard = (nrRows, nrCols) => {
           if (this.board[topRow][topCol + i] !== null) {
             throw new Error("Illegal ship placement");
           }
-          this.board[topRow][topCol + i] = "shipUnsunk";
+          this.board[topRow][topCol + i] = "shipUnhit";
+          ship.coords.push(JSON.stringify([topRow, topCol + i]));
         }
       } else {
         for (let i = 0; i < ship.length; i++) {
           if (this.board[topRow + i][topCol] !== null) {
             throw new Error("Illegal ship placement");
           }
-          this.board[topRow + i][topCol] = "shipUnsunk";
+          this.board[topRow + i][topCol] = "shipUnhit";
+          ship.coords.push(JSON.stringify([topRow + i, topCol]));
         }
       }
       this.ships.push(ship);
     },
+    receiveAttack(row, col) {
+      if (this.board[row][col] === "shipUnhit") {
+        this.board[row][col] = "shipHit";
+        const coords = JSON.stringify([row, col]);
+        this.ships.forEach((ship) => {
+          if (ship.coords.some((coord) => coord === coords)) {
+            ship.hit();
+            if (ship.isSunk()) {
+              console.log("Sunk ship!");
+            }
+          }
+        });
+      } else if (this.board[row][col] === null) {
+        this.board[row][col] = "hit";
+      } else {
+        throw new Error("Already hit");
+      }
+    }
   };
 };
 
--- index.test.js
+++ index.test.js
@@ -16,6 +16,17 @@ test("test createGameboard", () => {
   expect(gameboard.board[0].length).toBe(2);
   const ship = index.createShip(1);
   gameboard.placeShip(ship, 0, 0, true);
-  expect(gameboard.board[0][0]).toBe("shipUnsunk");
+  expect(gameboard.board[0][0]).toBe("shipUnhit");
   expect(gameboard.board[1][0]).toBe(null);
+});
+
+test("test receiveAttack", () => {
+  const gameboard = index.createGameboard(2, 2);
+  const ship = index.createShip(1);
+  gameboard.placeShip(ship, 0, 0, true);
+  gameboard.receiveAttack(0, 0);
+  gameboard.receiveAttack(1, 0);
+  expect(gameboard.board[0][0]).toBe("shipHit");
+  expect(ship.isSunk()).toBe(true);``
+  expect(gameboard.board[1][0]).toBe("hit");
 });
