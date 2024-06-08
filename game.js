35c3880c232ccf79a5384994ff38fbc897f5dedb
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-06-08 15:25:56 +0300

    Start setting up gameplay

--- game.js
+++ game.js
@@ -2,3 +2,6 @@ const index = require('./index.js');
 
 const player1 = index.createPlayer('human');
 const player2 = index.createPlayer('human');
+
+const gameboard1 = document.querySelector("#gameboard1");
+const gameboard2 = document.querySelector("#gameboard2");
