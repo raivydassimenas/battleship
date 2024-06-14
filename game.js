36ef1c91345ee0b1bd8c0b4eb30d36f14a1c078b
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-06-14 19:03:47 +0300

    Add event listener for making hits

--- game.js
+++ game.js
@@ -6,24 +6,55 @@ const colWidth = '1em';
 const player1 = createPlayer('human');
 const player2 = createPlayer('human');
 
+let move = 'Player 1';
+
 const renderGameboard = (gameboard, gameboardDiv) => {
     const nrRows = gameboard.nrRows;
     const nrCols = gameboard.nrCols;
-    gameboardDiv.style.display = 'grid';
-    gameboardDiv.style.gridTemplateRows = `repeat(${nrRows}, ${rowHeight})`;
-    gameboardDiv.style.gridTemplateColumns = `repeat(${nrCols}, ${colWidth})`
+    const gameboardDivChild = document.createElement('div');
+    gameboardDivChild.style.display = 'grid';
+    gameboardDivChild.style.gridTemplateRows = `repeat(${nrRows}, ${rowHeight})`;
+    gameboardDivChild.style.gridTemplateColumns = `repeat(${nrCols}, ${colWidth})`
 
     for (let i = 0; i < nrRows; i++) {
         for (let j = 0; j < nrCols; j++) {
             const square = document.createElement('div');
             square.classList.add('square');
-            gameboardDiv.appendChild(square);
+            if (gameboard.board[i][j] === 'shipHit') {
+                square.style.background = 'red';
+            } else if (gameboard.board[i][j] === 'hit') {
+                square.style.background = 'black';
+            } else {
+                square.addEventListener('click', e => {
+                    if (gameboard === player1.gameboard && move === 'Player 2') {
+                        gameboard.receiveAttack(i, j);
+                        move = 'Player 1';
+                    } else if (gameboard === player2.gameboard && move === 'Player 1') {
+                        gameboard.receiveAttack(i, j);
+                        move = 'Player 2';
+                    }
+                    moveDiv.textContent = move;
+                    gameboardDiv.innerHTML = '';
+                    renderGameboard(gameboard, gameboardDiv);
+                });
+            }
+            gameboardDivChild.appendChild(square);
         }
+    }
+    gameboardDiv.appendChild(gameboardDivChild);
+}
+
+const placeShips = (ships, player) => {
+    for (let ship in ships) {
+
     }
 }
 
-const gameboard1 = document.querySelector("#gameboard1");
-const gameboard2 = document.querySelector("#gameboard2");
+
+const gameboardDiv1 = document.querySelector("#gameboard1");
+const gameboardDiv2 = document.querySelector("#gameboard2");
+const moveDiv = document.querySelector('#move');
+moveDiv.textContent = move;
 
 renderGameboard(player1.gameboard, gameboard1);
 renderGameboard(player2.gameboard, gameboard2);
